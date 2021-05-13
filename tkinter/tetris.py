f11fe29ca0df0c524b19d233e6dd41fe9601f28f
Joey Oswald <rapitwitterbots@gmail.com>
2021-05-12 20:45:41 -0400

    added end game functionality

--- tkinter/tetris.py
+++ tkinter/tetris.py
@@ -9,7 +9,7 @@ class GameBoard(tk.Canvas):
 
 class Block():
 
-    COLORS = ['black', 'red', 'green', 'blue', 'cyan', 'magenta']
+    COLORS = ['red', 'green', 'blue', 'cyan', 'magenta']
 
     def __init__(self, canvas, coords, color, start_point=[1, 1]):
         # creating a rectangle on the provided canvas with a specified color
@@ -18,9 +18,10 @@ class Block():
         self.start_point = start_point
         self.coords = coords
         self.offset_coords = self.add_offset(self.coords, self.start_point)
+        # Once coordinates are obtained, draw the block on screen with tags
         self.id = self.canvas.create_rectangle(self.offset_coords, fill=self.color,
                                                tags=("active", "block"))
-        print(str(self.id) + " coordinates: " + str(self.canvas.coords(self.id)))
+        # print(str(self.id) + " coordinates: " + str(self.canvas.coords(self.id)))
 
     def add_offset(self, coords, start_point):
         # Convert the start point to offset coordinates
@@ -31,7 +32,11 @@ class Block():
         y_top = (coords[1] + y_off)
         x_right = (coords[2] + x_off)
         y_bottom = (coords[3] + y_off)
-        return (x_left, y_top, x_right, y_bottom)
+        # Error validation - make sure piece is on screen
+        if x_left < 0 or x_right > Tetris.GAME_WIDTH or y_top < 0 or y_bottom > 60:
+            return (1, 1, 16, 16)
+        else:
+            return (x_left, y_top, x_right, y_bottom)
 
 
 class Piece():
@@ -47,14 +52,13 @@ class Piece():
             Block(self.canvas, coords, color=self.color,
                   start_point=self.start_point)
 
-    def deactivate_all(self):
+    def deactivate_all(self):  # Removes active tag from everything
         self.canvas.dtag("active", "active")
 
     def choose_color(self):
         return random.choice(Block.COLORS)
 
-    def choose_matrix(self, piece_shape):
-        # Series of if statements chooses which shape to return
+    def choose_matrix(self, piece_shape):  # Series of if statements chooses which shape to return
         if piece_shape in ["line", Tetris.LINE]:
             self.matrix = Tetris.LINE
             return self.matrix
@@ -97,30 +101,30 @@ class Tetris():
             [(2 * s), 0, (3 * s), s],
             [(3 * s), 0, (4 * s), s]]
 
-    RIGHT_L = [[0, s, s, (2 * s)],
-               [0, (2 * s), s, (s)],
-               [0, (s), s, (4 * s)],
-               [s, (s), (2 * s), (4 * s)]]
+    RIGHT_L = [[0, 0, s, s],
+               [0, s, s, (2 * s)],
+               [0, (2 * s), s, (3 * s)],
+               [s, (2 * s), (2 * s), (3 * s)]]
 
-    LEFT_L = [[s, s, (2 * s), (2 * s)],
-              [s, (2 * s), (2 * s), (s)],
-              [s, (s), (2 * s), (4 * s)],
-              [0, (s), s, (4 * s)]]
+    LEFT_L = [[s, 0, (2 * s), s],
+              [s, s, (2 * s), (2 * s)],
+              [s, (2 * s), (2 * s), (3 * s)],
+              [0, (2 * s), s, (3 * s)]]
 
     T_SHAPE = [[0, 0, s, s],
                [s, 0, (2 * s), s],
-               [(2 * s), 0, (s), s],
+               [(2 * s), 0, (3 * s), s],
                [s, s, (2 * s), (2 * s)]]
 
     S_SHAPE = [[0, s, s, (2 * s)],
                [s, 0, (2 * s), s],
-               [(2 * s), 0, (s), s],
-               [s, s, (2 * s), (2 * s)]]
+               [s, s, (2 * s), (2 * s)],
+               [(2 * s), 0, (3 * s), s]]
 
     Z_SHAPE = [[0, 0, s, s],
                [s, 0, (2 * s), s],
                [s, s, (2 * s), (2 * s)],
-               [(2 * s), s, (s), (2 * s)]]
+               [(2 * s), s, (3 * s), (2 * s)]]
 
     SQUARE = [[0, 0, s, s],
               [s, 0, (2 * s), s],
@@ -130,15 +134,12 @@ class Tetris():
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Tetris")
-        self.root.geometry("360x460")
+        self.root.geometry("360x490")
         self.root.bind("<Key>", self.set_controls)  # Anytime a key is pressed, runs this function
 
     def start_game(self):
-        # will be a mainloop
         self.canvas = self.create_canvas()
-        Piece(self.canvas, [280, 16], self.SQUARE)
-        Piece(self.canvas, piece_shape=self.LINE)
-        self.root.after(self.GAME_SPEED, None)
+        self.drop()
         self.root.mainloop()
 
     def set_controls(self, event):
@@ -150,32 +151,33 @@ class Tetris():
                 # Finds all objects with "active" tag, then for each block, moves them
                 for block_id in active_piece:
                     self.canvas.move(block_id, -self.BLOCK_SIZE, 0)
-            self.check_below()
+
         if event.keysym == "Up":  # Up Arrow
             pass
         if event.keysym == "Down":
             active_piece = self.canvas.find_withtag("active")
-            # Finds all objects with "active" tag, then for each block, moves them
-            for block_id in active_piece:
-                self.canvas.move(block_id, 0, self.BLOCK_SIZE)
-            self.check_below()
+            if self.check_below([0, self.BLOCK_SIZE]):
+                # Finds all objects with "active" tag, then for each block, moves them
+                for block_id in active_piece:
+                    self.canvas.move(block_id, 0, self.BLOCK_SIZE)
         if event.keysym == "Right":  # Right Arrow
             ok_to_move = self.check_move([self.BLOCK_SIZE, 0])
             if ok_to_move:
                 active_piece = self.canvas.find_withtag("active")
                 for block_id in active_piece:
                     self.canvas.move(block_id, self.BLOCK_SIZE, 0)
-            self.check_below()
 
-    def create_canvas(self):
-        # create the canvas which will actually house the game
+    def create_canvas(self):  # create the canvas which will actually house the game
         self.canvas = GameBoard(self.root, width=self.GAME_WIDTH, height=self.GAME_HEIGHT,
                                 bg="white")
         self.canvas.grid(row=0, rowspan=2, column=0, columnspan=2, padx=3, pady=3)
         return self.canvas
 
-    def check_move(self, coords_change=[]):
-        # Will return False if the move brings the piece in contact with a boundary
+    def choose_piece(self):
+        return random.choice([self.LINE, self.RIGHT_L, self.LEFT_L, self.SQUARE,
+                              self.S_SHAPE, self.Z_SHAPE, self.T_SHAPE])
+
+    def check_move(self, coords_change=[]):  # returns False if the piece will contact a boundary
         active_piece = self.canvas.find_withtag("active")  # Find piece
         can_move = False
         for block_id in active_piece:
@@ -190,6 +192,10 @@ class Tetris():
             y_top_new = y_top + coords_change[1]
             x_right_new = x_right + coords_change[0]
             y_bottom_new = y_bottom + coords_change[1]
+            # If new coords are out of bounds, return false
+            if x_left_new < 0 or y_bottom_new >= self.GAME_HEIGHT or x_right_new > self.GAME_WIDTH:
+                can_move = False
+                return False
             # Going to create a box with coordinate change factored in to
             # check if that box is tangent to any other piece
             # Subtract 1 b/c canvas automatically adds 1 to box size when it makes it
@@ -197,22 +203,20 @@ class Tetris():
                                         x_left_new + 1,
                                         y_top_new,
                                         x_right_new - 1,
-                                        y_bottom_new)
+                                        y_bottom_new - 1)
         # Add the expected coordinate changes to mins/maxes and check
+        # If any blocks have a non-active overlapping piece, return False
             for i in range(len(overlapping)):
-                if overlapping[i] not in active_piece or x_left_new < 0 \
-                   or y_bottom_new >= self.GAME_HEIGHT or x_right_new > self.GAME_WIDTH:
-                    print(overlapping)
-                    can_move = False
-                    break
+                if overlapping[i] not in active_piece:
+                    return False
                 else:
                     can_move = True
-        print(can_move)
+                    # Can't return true, or else loop will terminate before all options
         return can_move
 
-    def check_below(self):
-        # Will return True if another piece is below
+    def check_below(self, coords_change=[]):  # Will return True if another piece is below
         active_piece = self.canvas.find_withtag("active")  # Find piece
+        can_move = False
         for block_id in active_piece:
             # For each block, get its coordinates
             block_coords = self.canvas.coords(block_id)
@@ -220,18 +224,60 @@ class Tetris():
             y_top = block_coords[1]
             x_right = block_coords[2]
             y_bottom = block_coords[3]
+            if y_bottom >= self.GAME_HEIGHT:
+                for block_id in active_piece:
+                    self.canvas.dtag(block_id, "active")
+                return False
+            # All coordinates with the coordinate change factored in
+            x_left_new = x_left + coords_change[0]
+            y_top_new = y_top + coords_change[1]
+            x_right_new = x_right + coords_change[0]
+            y_bottom_new = y_bottom + coords_change[1]
             # Going to find if any pieces are tangent to bottom coordinates
             overlapping = self.canvas.find_overlapping(
-                                        (x_left + 1),
-                                        y_top,
-                                        (x_right - 1),
-                                        y_bottom)
+                                        x_left_new + 1,
+                                        y_top_new,
+                                        x_right_new - 1,
+                                        y_bottom_new - 1)
             for i in range(len(overlapping)):
                 if overlapping[i] not in active_piece:
-                    print("piece being deactivated")
+                    # deactivates the piece
                     for block_id in active_piece:
                         self.canvas.dtag(block_id, "active")
-                    print("piece deactivated")
+                    return False
+                else:
+                    can_move = True
+        return can_move
+
+    def game_over(self):  # Returns true for game over
+        active_piece = self.canvas.find_withtag("active")  # Find piece
+        tops = []
+        # Going to grab each block's top coordinate
+        for block_id in active_piece:
+            block_coords = self.canvas.coords(block_id)
+            tops.append(block_coords[1])
+        # loss conditions = piece can't move down, is at top
+        if (0 in tops or 1 in tops) and not self.check_below([0, self.BLOCK_SIZE]):
+            return True
+        else:
+            return False
+
+    def end_game(self):
+        print("game over!")
+
+    def drop(self):
+        active_piece = self.canvas.find_withtag("active")  # Find piece
+        if not active_piece:
+            Piece(self.canvas, piece_shape=self.LINE)
+            # Piece(self.canvas, piece_shape=self.choose_piece())
+        if not self.game_over():
+            if self.check_below([0, self.BLOCK_SIZE]):
+                # Finds all objects with "active" tag, then for each block, moves them
+                for block_id in active_piece:
+                    self.canvas.move(block_id, 0, self.BLOCK_SIZE)
+        else:
+            return
+        self.root.after(50, self.drop)
 
 
 def main():
